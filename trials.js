1b932ec4cedef56fd38752e47e2fb1994ef18dcb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-11-03 12:36:26 -0700

    Initial commit

---
+++ trials.js
@@ -0,0 +1,100 @@
+/////////////////////////////////////////////////////////
+// PART 1
+
+/////////////////////////////////////////////////////////
+// Show Profile Info
+
+// Add function to prnt profile info
+function showProfile(name, catchphrase, location) {
+	console.log('PROFILE:');
+	console.log('Name: ' + name);
+	console.log('Catchphrase: ' + catchphrase);
+	console.log('Location: ' + location);
+}
+
+/////////////////////////////////////////////////////////
+// Display Likes
+const interests = [
+  'being annoying',
+  'seeds',
+  'staring at human food'
+];
+
+// Add function to print interests
+function showLikes(like){
+	console.log('THINKS I LIKE:');
+
+	for (let item = 0; item < like.length(); item += 1) {
+		console.log(like[item]);
+	}
+}
+
+
+/////////////////////////////////////////////////////////
+// Display Favorites
+const favorites = {
+  // Add more favorites
+};
+
+// Add function to print favorites
+
+
+
+/////////////////////////////////////////////////////////
+// PART 2
+
+/////////////////////////////////////////////////////////
+// Add Transactions
+
+// Add function to add transaction
+
+// Create object to hold transactions
+
+// Add transactions to object
+
+
+/////////////////////////////////////////////////////////
+// Get Balance Status
+
+// Add function to calculate balance status
+
+
+
+/////////////////////////////////////////////////////////
+// Calculate Current Balance
+
+// Add function to calculate and return current balance
+
+
+/////////////////////////////////////////////////////////
+// Show Account Activity
+
+// Add function to print account activity
+
+
+
+/////////////////////////////////////////////////////////
+// Creating a User Object
+
+// Add user object
+
+// Add function to print user dashboard
+
+
+
+/////////////////////////////////////////////////////////
+// PART 3
+
+// Add object that keeps track of premium members
+
+// Add function to return emails of premium members
+
+
+
+/////////////////////////////////////////////////////////
+// PART 4
+
+// Add function to assign interns to workdays
+
+// Add a function to generate customer support hours
+
