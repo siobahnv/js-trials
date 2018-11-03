40b4f4f8564dbdb4ac873afcb9074ceef8175fe0
Hackbright Student <no-reply@hackbrightacademy.com>
2018-11-03 16:00:04 -0700

    Finish through Show-Account-Activity

--- trials.js
+++ trials.js
@@ -24,7 +24,7 @@ const interests = [
 function showLikes(like){
 	console.log('THINKS I LIKE:');
 
-	for (let item = 0; item < like.length(); item += 1) {
+	for (let item = 0; item < like.length; item += 1) {
 		console.log(like[item]);
 	}
 }
@@ -34,10 +34,19 @@ function showLikes(like){
 // Display Favorites
 const favorites = {
   // Add more favorites
+  'food': 'pebbles',
+  'tree': 'palm',
+  'quote': 'Bock bock bock'
 };
 
 // Add function to print favorites
+function showFavorites(favorites) {
+	console.log('FAVORITES:');
 
+	for (let key of Object.keys(favorites)) {
+		console.log(`${key}: ${favorites[key]}`);
+	}
+}
 
 
 /////////////////////////////////////////////////////////
@@ -47,30 +56,64 @@ const favorites = {
 // Add Transactions
 
 // Add function to add transaction
-
+function addTransaction(date, amount, currentTransactions){
+	currentTransactions[date] = amount;
+}
 // Create object to hold transactions
+let transactions = {};
 
 // Add transactions to object
-
+addTransaction('may2', -500, transactions)
+addTransaction('may13', 1200, transactions)
+addTransaction('may15', -100, transactions)
 
 /////////////////////////////////////////////////////////
 // Get Balance Status
 
 // Add function to calculate balance status
-
+function getBalanceStatus(balance) {
+	if (balance < 0) {
+		console.log('YOU ARE OVERDRAWN');
+	} else if (balance < 20) {
+		console.log('Warning! Your balance is almost 0!');
+	} else {
+		console.log('Normal')
+	}
+}
 
 
 /////////////////////////////////////////////////////////
 // Calculate Current Balance
 
 // Add function to calculate and return current balance
+function getCurrentBalance(balance, transactions) {
+	for (let key of Object.keys(transactions)) {
+		balance += transactions[key];
+		if(balance < 0) {
+			balance -=25;
+		}
+	}
 
+	return balance;
+}
 
 /////////////////////////////////////////////////////////
 // Show Account Activity
 
 // Add function to print account activity
+function showAcctActivity(balance, transactions) {
+	console.log('TRANSACTIONS:');
+	for (let key of Object.keys(transactions)) {
+		console.log(`${key}: ${transactions[key]}`)
+	}
 
+	let currentBalance = getCurrentBalance(balance, transactions);
+
+	console.log('CURRENT BALANCE: ' + currentBalance);
+
+	console.log('CURRENT STATUS: ');
+	getBalanceStatus(currentBalance);
+}
 
 
 /////////////////////////////////////////////////////////
