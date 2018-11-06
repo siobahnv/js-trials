35ac75a0b5792f789bfc7c0ac4b85fb835ee5ccb
siobahnv <siobahnv@gmail.com>
2018-11-05 19:39:51 -0800

    Add user object.

--- trials.js
+++ trials.js
@@ -6,46 +6,46 @@
 
 // Add function to prnt profile info
 function showProfile(name, catchphrase, location) {
-	console.log('PROFILE:');
-	console.log('Name: ' + name);
-	console.log('Catchphrase: ' + catchphrase);
-	console.log('Location: ' + location);
+    console.log('PROFILE:');
+    console.log('Name: ' + name);
+    console.log('Catchphrase: ' + catchphrase);
+    console.log('Location: ' + location);
 }
 
 /////////////////////////////////////////////////////////
 // Display Likes
 const interests = [
-  'being annoying',
-  'seeds',
-  'staring at human food'
+    'being annoying',
+    'seeds',
+    'staring at human food'
 ];
 
 // Add function to print interests
-function showLikes(like){
-	console.log('THINKS I LIKE:');
+function showLikes(like) {
+    console.log('THINKS I LIKE:');
 
-	for (let item = 0; item < like.length; item += 1) {
-		console.log(like[item]);
-	}
+    for (let item = 0; item < like.length; item += 1) {
+        console.log(like[item]);
+    }
 }
 
 
 /////////////////////////////////////////////////////////
 // Display Favorites
 const favorites = {
-  // Add more favorites
-  'food': 'pebbles',
-  'tree': 'palm',
-  'quote': 'Bock bock bock'
+    // Add more favorites
+    'food': 'pebbles',
+    'tree': 'palm',
+    'quote': 'Bock bock bock'
 };
 
 // Add function to print favorites
 function showFavorites(favorites) {
-	console.log('FAVORITES:');
+    console.log('FAVORITES:');
 
-	for (let key of Object.keys(favorites)) {
-		console.log(`${key}: ${favorites[key]}`);
-	}
+    for (let key of Object.keys(favorites)) {
+        console.log(`${key}: ${favorites[key]}`);
+    }
 }
 
 
@@ -56,8 +56,8 @@ function showFavorites(favorites) {
 // Add Transactions
 
 // Add function to add transaction
-function addTransaction(date, amount, currentTransactions){
-	currentTransactions[date] = amount;
+function addTransaction(date, amount, currentTransactions) {
+    currentTransactions[date] = amount;
 }
 // Create object to hold transactions
 let transactions = {};
@@ -72,13 +72,13 @@ addTransaction('may15', -100, transactions)
 
 // Add function to calculate balance status
 function getBalanceStatus(balance) {
-	if (balance < 0) {
-		console.log('YOU ARE OVERDRAWN');
-	} else if (balance < 20) {
-		console.log('Warning! Your balance is almost 0!');
-	} else {
-		console.log('Normal')
-	}
+    if (balance < 0) {
+        console.log('YOU ARE OVERDRAWN');
+    } else if (balance < 20) {
+        console.log('Warning! Your balance is almost 0!');
+    } else {
+        console.log('Normal')
+    }
 }
 
 
@@ -87,14 +87,14 @@ function getBalanceStatus(balance) {
 
 // Add function to calculate and return current balance
 function getCurrentBalance(balance, transactions) {
-	for (let key of Object.keys(transactions)) {
-		balance += transactions[key];
-		if(balance < 0) {
-			balance -=25;
-		}
-	}
+    for (let key of Object.keys(transactions)) {
+        balance += transactions[key];
+        if (balance < 0) {
+            balance -= 25;
+        }
+    }
 
-	return balance;
+    return balance;
 }
 
 /////////////////////////////////////////////////////////
@@ -102,27 +102,38 @@ function getCurrentBalance(balance, transactions) {
 
 // Add function to print account activity
 function showAcctActivity(balance, transactions) {
-	console.log('TRANSACTIONS:');
-	for (let key of Object.keys(transactions)) {
-		console.log(`${key}: ${transactions[key]}`)
-	}
+    console.log('TRANSACTIONS:');
+    for (let key of Object.keys(transactions)) {
+        console.log(`${key}: ${transactions[key]}`)
+    }
 
-	let currentBalance = getCurrentBalance(balance, transactions);
+    let currentBalance = getCurrentBalance(balance, transactions);
 
-	console.log('CURRENT BALANCE: ' + currentBalance);
+    console.log('CURRENT BALANCE: ' + currentBalance);
 
-	console.log('CURRENT STATUS: ');
-	getBalanceStatus(currentBalance);
+    console.log('CURRENT STATUS: ');
+    getBalanceStatus(currentBalance);
 }
 
 
 /////////////////////////////////////////////////////////
 // Creating a User Object
+let user = {
+    'name': 'melon',
+    catchphrase: 'woot!',
+    location: 'San Francisco',
+    interests: interests,
+    favorites: favorites,
+    transactions: transactions,
+    start_balance: 100
+};
 
 // Add user object
 
 // Add function to print user dashboard
-
+function printDashboard(userObj) {
+    console.log(userObj);
+}
 
 
 /////////////////////////////////////////////////////////
@@ -140,4 +151,3 @@ function showAcctActivity(balance, transactions) {
 // Add function to assign interns to workdays
 
 // Add a function to generate customer support hours
-
